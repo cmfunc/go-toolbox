79d84d48a5aea4cbcd4d05722205d41b1789b04d
mingchen@ahaent.com <mingchen@ahaent.com>
2023-03-02 16:17:25 +0800

    maint:添加分页查询时数据库位移参数转换

---
+++ page_offset/page.go
@@ -0,0 +1,18 @@
+package pageoffset
+
+import "context"
+
+// TransOffset
+// 接口列表分页与数据库分页查询参数转换
+// @param index 页码
+// @param size 页面数据个数
+func TransOffset(ctx context.Context, index, size uint64) (offset, limit uint64) {
+	offset, limit = 0, 10
+	if size > 0 {
+		limit = size
+	}
+	if index > 0 {
+		offset = (index - 1) * limit
+	}
+	return offset, limit
+}
